71c972819368ecf563cdce1ac43b368b492bb460
NilsHolger <nils.holger.naegele@gmail.com>
2018-09-07 10:01:10 +0200

    styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,35 @@ import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>Fetch & Merge Data</h1>
-  <code class="format">{{ userDataComments | json }}</code>
-  `,
-  styles: [`
-      .format {
-        white-space: pre-wrap;
-      }
-  `]
+  <div class="container">
+  <h1>User Data</h1>
+  <div class="card">
+  <div class="card-body">
+    <h5 class="card-title">Name: {{ userDataComments?.User.name }}</h5>
+    <p class="card-text">UserName: {{ userDataComments?.User.username }}</p>
+    <p class="card-text">Email: {{ userDataComments?.User.email }}</p>
+    <p class="card-text">Address: {{ userDataComments?.User.address.street }},
+    {{ userDataComments?.User.address.suite }}, {{ userDataComments?.User.address.city }},
+    {{ userDataComments?.User.address.zipcode }},
+    latitude: {{ userDataComments?.User.address.geo.lat }}
+    longitude: {{ userDataComments?.User.address.geo.lng }}
+    </p>
+    <p class="card-text">Phone: {{ userDataComments?.User.phone }}</p>
+    <p class="card-text">Website: {{ userDataComments?.User.website }}</p>
+    <p class="card-text">Company: {{ userDataComments?.User.company.name }},
+                                  {{ userDataComments?.User.company.catchPhrase }},
+                                  {{ userDataComments?.User.company.bs }}
+    </p>
+    <h3>Comments</h3>
+    <div class="alert alert-primary" role="alert"
+    *ngFor="let comments of userDataComments?.Comments">
+         <span class="d-block p-2 bg-primary text-white">{{ comments.title }}</span>
+          <span>{{ comments.body }}</span>
+    </div>
+  </div>
+</div>
+  </div>
+  `
 })
 export class AppComponent implements OnInit, OnDestroy {
 
@@ -30,7 +51,7 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   private mergeUserData(userCommentsData: any): any {
-    return userCommentsData ? { User: { ...userCommentsData[0]}, Coments: userCommentsData[1] }
+    return userCommentsData ? { User: { ...userCommentsData[0]}, Comments: userCommentsData[1] }
                             : userCommentsData;
   }
 
